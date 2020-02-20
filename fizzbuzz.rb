6d62bab6de397c5095a403d229356134f207d221
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-20 05:59:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,13 @@ def fizzbuzz
 end
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 
+    "Fizz" 
+  end
+end
+
+def fizzbuzz(int)
+  if int % 5 == 0 # if the number int is divisible by 3
+    "Buzz" # Go fizz
   end
 end
